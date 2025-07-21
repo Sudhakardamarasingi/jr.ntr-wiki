ea9410e405c23ac5149f086b7f698559ddd072f0
Sudhakar Damarasingi <damarasingisudhakar@gmail.com>
2025-07-21 22:09:47 +0530

    ntr.py

--- ntr.py
+++ ntr.py
@@ -83,6 +83,7 @@ from PIL import Image
 def resize_image(img_path, size=(300, 400)):
     img = Image.open(img_path)
     return img.resize(size)
+
 elif page == "Anticipated movies":
     col1,col2,col3,col4=st.columns(4)
     with col1:
